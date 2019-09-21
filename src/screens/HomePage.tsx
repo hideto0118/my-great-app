7248bf83a29251d2a8dfac4baa444fbb3c7cda72
Hideto Yasunori <hideto0118yasunori@gmail.com>
2019-09-21 15:52:11 -0700

    Change h1

--- src/screens/HomePage.tsx
+++ src/screens/HomePage.tsx
@@ -16,7 +16,7 @@ const AsideImageOuter = styled.figure`
 const HomePage: React.FC = () => {
   return (
     <DefaultLayout className="HomePage">
-      <h1>Welcome to My Great App!</h1>
+      <h1>こんにちはー!</h1>
       <p>
         Hi World!
         My name is Ginpei Takanashi.
